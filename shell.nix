e93c47f518fa5647e706ef9bd219382d09e7f366
kintrix007 <kintrix007@proton.me>
2024-01-12 20:36:56 +0100

    Configured template

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  url = "https://github.com/NixOS/nixpkgs/archive/08d0afbbdf91192d8753bca6c0f492bd58070400.tar.gz";
+in
+{ pkgs ? import (fetchTarball url) { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    nodePackages.typescript-language-server
+    nodejs
+  ];
+}
