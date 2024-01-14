e7d52d84cabf263bc07b4966196864ea09eaadf9
kintrix007 <kintrix007@proton.me>
2024-01-14 21:18:41 +0100

    Add styling to popup

--- components/popup.tsx
+++ components/popup.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from "react";
+import styles from "./popup.module.scss";
 
-export function Popup({ title, children }: { title: string, children: ReactNode }) {
-    return <div>
+export function Popup(
+    { title, children, onClose }: {
+        title: string;
+        children: ReactNode;
+        onClose?: () => void;
+    }
+) {
+    return <div className={styles.popup}>
+        <h3>{title}</h3>
         {children}
     </div>;
 }
